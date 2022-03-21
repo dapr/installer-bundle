d91010d82cb70cad91174795d4846f2e485794ef
Shivam Kumar <shivamkm07@gmail.com>
2022-03-21 09:57:02 +0530

    Release workflow and scripts (#1)
    * Release workflow and scripts
    
    Signed-off-by: shivam <shivamkm07@gmail.com>
    
    * Using DAPR_BOT_TOKEN
    
    Signed-off-by: shivam <shivamkm07@gmail.com>
    
    * Docker image for darwin
    
    Signed-off-by: shivam <shivamkm07@gmail.com>
    
    * Updating README
    
    Signed-off-by: shivam <shivamkm07@gmail.com>

---
+++ .github/scripts/build_daprbundle.py
@@ -0,0 +1,218 @@
+# ------------------------------------------------------------
+# Copyright 2021 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------
+
+
+import argparse
+from errno import ESTALE
+from fileinput import filename
+from http.client import OK
+import subprocess
+import tarfile
+import zipfile
+import requests
+import json
+import os
+import sys
+import shutil
+
+
+# GitHub Organization and repo name to download release
+GITHUB_ORG="dapr"
+GITHUB_DAPR_REPO="dapr"
+GITHUB_DASHBOARD_REPO="dashboard"
+GITHUB_CLI_REPO="cli"
+
+# Dapr binaries filename
+DAPRD_FILENAME="daprd"
+PLACEMENT_FILENAME="placement"
+DASHBOARD_FILENAME="dashboard"
+CLI_FILENAME="dapr"
+DAPRBUNDLE_FILENAME="daprbundle"
+
+BIN_DIR="dist"
+IMAGE_DIR="docker"
+BUNDLE_DIR="daprbundle"
+ARCHIVE_DIR="archive"
+
+global runtime_os,runtime_arch,runtime_ver,dashboard_ver,cli_ver
+
+def getLatestRelease(repo):
+    daprReleaseUrl = "https://api.github.com/repos/" + GITHUB_ORG + "/" + repo + "/releases"
+    print(daprReleaseUrl)
+    latest_release = ""
+    resp = requests.get(daprReleaseUrl)
+    if resp.status_code != requests.codes.ok:
+        print(f"Error pulling latest release of {repo}")
+        resp.raise_for_status()
+        sys.exit(1)
+    data = json.loads(resp.text)
+    version = data[0]['tag_name'].lstrip("v")
+    print(version)
+    return version
+
+def binaryFileName(fileBase):
+    if(runtime_os == "windows"):
+        ext = "zip"
+    else:
+        ext = "tar.gz"
+    fileName = f"{fileBase}_{runtime_os}_{runtime_arch}.{ext}"
+    return fileName
+
+def make_archive(src,dest,fileBase):
+    print(f"Archiving {src} to {os.path.join(dest,binaryFileName(fileBase))}")
+    fileNameBase = f"{fileBase}_{runtime_os}_{runtime_arch}"
+    filePathBase = os.path.join(dest,fileNameBase)
+    if runtime_os == "windows":
+        shutil.make_archive(filePathBase,"zip",".",src)
+    else:
+        shutil.make_archive(filePathBase,"gztar",".",src)
+
+
+def unpack_archive(filePath,dir):
+    print(f"Extracting {filePath} to {dir}")
+    if filePath.endswith('.zip'):
+        shutil.unpack_archive(filePath,dir,"zip")
+    else:
+        if filePath.endswith('.tar.gz'):
+            shutil.unpack_archive(filePath,dir,"gztar")
+        else:
+            print(f"Unknown archive file {filePath}")
+            sys.exit(1)
+
+
+def downloadBinary(repo, fileBase, version, out_dir):
+    fileName = binaryFileName(fileBase)
+    url = f"https://github.com/{GITHUB_ORG}/{repo}/releases/download/v{version}/{fileName}"
+    downloadPath = os.path.join(out_dir,fileName)
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    print(f"Downloading {url} to {downloadPath}")
+
+    resp = requests.get(url,stream=True)
+    if resp.status_code == 200:
+        with open(downloadPath, 'wb') as f:
+            f.write(resp.raw.read())
+    else:
+        print(f"Error: Unable to Download {url}")
+
+    print(f"Downloaded {url} to {downloadPath}")
+
+def downloadBinaries(dir):
+    bin_dir = os.path.join(dir,BIN_DIR)
+    downloadBinary(GITHUB_DAPR_REPO,DAPRD_FILENAME,runtime_ver,bin_dir)
+    downloadBinary(GITHUB_DAPR_REPO,PLACEMENT_FILENAME,runtime_ver,bin_dir)
+    downloadBinary(GITHUB_DASHBOARD_REPO,DASHBOARD_FILENAME,dashboard_ver,bin_dir)
+    downloadBinary(GITHUB_CLI_REPO,CLI_FILENAME,cli_ver,dir)
+
+    cli_filepath = os.path.join(dir,binaryFileName(CLI_FILENAME))
+    unpack_archive(cli_filepath,dir)
+    os.remove(cli_filepath)
+
+
+def downloadDockerImage(image_name, version, out_dir):
+    docker_image=f"{image_name}:{version}"
+    if (version == "latest"):
+        docker_image =image_name
+    fileName= docker_image.replace("/","-").replace(":","-") + ".tar.gz"
+    downloadPath = os.path.join(out_dir,fileName)
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    print(f"Downloading {docker_image} to {downloadPath}")
+
+    cmd = ["docker", "pull",docker_image]
+    completed_process = subprocess.run(cmd,text=True)
+    if(completed_process.returncode != 0):
+        print(f"Error pulling docker image {docker_image}")
+        sys.exit(1)
+    
+    cmd = ["docker", "save", "-o", downloadPath, docker_image]
+    completed_process = subprocess.run(cmd,text=True)
+    if(completed_process.returncode != 0):
+        print(f"Error saving docker image {docker_image}")
+        sys.exit(1)
+
+    print(f"Downloaded {docker_image} to {downloadPath}")
+
+def downloadDockerImages(dir):
+    image_dir = os.path.join(dir,IMAGE_DIR)
+    downloadDockerImage("daprio/dapr",runtime_ver,image_dir)
+
+def parseArguments():
+    global runtime_os,runtime_arch,runtime_ver,dashboard_ver,cli_ver,ARCHIVE_DIR
+    all_args = argparse.ArgumentParser()
+    all_args.add_argument("--runtime_os",required=True,help="Runtime OS: [windows/linux/darwin]")
+    all_args.add_argument("--runtime_arch",required=True,help="Runtime Architecture: [amd64/arm/arm64]")
+    all_args.add_argument("--runtime_ver",default="latest",help="Dapr Runtime Version: default=latest e.g. 1.6.0")
+    all_args.add_argument("--dashboard_ver",default="latest",help="Dapr Dashboard Version: default=latest e.g. 0.9.0")
+    all_args.add_argument("--cli_ver",default="latest",help="Dapr CLI Version: default=latest e.g. 1.6.0")
+    all_args.add_argument("--archive_dir",default="archive",help="Output Archive directory: default=archive")
+
+    args = vars(all_args.parse_args())
+    runtime_os = str(args['runtime_os'])
+    runtime_arch = str(args['runtime_arch'])
+    runtime_ver = str(args["runtime_ver"])
+    dashboard_ver = str(args["dashboard_ver"])
+    cli_ver = str(args["cli_ver"])
+    ARCHIVE_DIR = str(args["archive_dir"])
+
+    if runtime_ver == "latest":
+        runtime_ver = getLatestRelease(GITHUB_DAPR_REPO)
+    if dashboard_ver == "latest":
+        dashboard_ver = getLatestRelease(GITHUB_DASHBOARD_REPO)
+    if cli_ver == "latest":
+        cli_ver = getLatestRelease(GITHUB_CLI_REPO)
+
+def deleteIfExists(dir):
+    if os.path.exists(dir):
+        if os.path.isdir(dir):
+            shutil.rmtree(dir)
+        else:
+            os.remove(dir)
+
+def write_version(dir):
+    versions = {
+        "daprd" : runtime_ver,
+        "dashboard": dashboard_ver,
+        "cli": cli_ver
+    }
+    jsonString = json.dumps(versions)
+    filePath = os.path.join(dir,"version.json")
+    with open(filePath,'w') as f:
+        f.write(jsonString)
+
+
+#############Main###################
+
+#Parsing Arguments
+parseArguments()
+
+#Cleaning the bundle and archive  directory
+deleteIfExists(BUNDLE_DIR)
+deleteIfExists(ARCHIVE_DIR)
+
+out_dir = BUNDLE_DIR
+#Downloading Binaries
+downloadBinaries(out_dir)
+
+#Downloading Docker images
+downloadDockerImages(out_dir)
+
+#writing versions
+write_version(out_dir)
+
+#Archiving bundle
+make_archive(BUNDLE_DIR,ARCHIVE_DIR,DAPRBUNDLE_FILENAME)
--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# installer-bundle
-Dapr bundled installer containing CLI and runtime packaged together. This eliminated the need to download Docker images when initializing Dapr locally.
+# Dapr Installer Bundle
+Dapr Installer Bundle contains CLI, runtime and dashboard packaged together. This eliminates the need to download binaries as well as docker images when initializing Dapr locally.
+
+## Setup
+Each release of Dapr Installer Bundle includes various OSes and architectures. These packages can be manually downloaded and used to initialize dapr locally.
+
+1. Download the [Dapr Installer Bundle](https://github.com/dapr/installer-bundle/releases)
+2. Unpack it (e.g. daprbundle_linux_amd64.tar.gz, daprbundle_windows_amd64.zip)
+3. To install Dapr CLI for further use, copy `daprbundle/dapr(.exe for windows)` binary to the desired location:
+   * For Linux/MacOS - `/usr/local/bin`
+   * For Windows, create a directory and add this to your System PATH. For example create a directory called `c:\dapr` and add this directory to your path, by editing your system environment variable.
+
+### Install Dapr on your local machine (self-hosted)
+
+In self-hosted mode, dapr can be initialized using the CLI  with the placement container enabled by default(recommended) or without it(slim installation) which also does not require docker to be available in the environment.
+
+#### Initialize Dapr
+
+([Prerequisite](#Prerequisites): Docker is available in the environment - recommended)
+
+Use the init command to initialize Dapr. On init, multiple default configuration files and containers are installed along with the dapr runtime binary. Dapr runtime binary is installed under $HOME/.dapr/bin for Mac, Linux and %USERPROFILE%\.dapr\bin for Windows.
+
+Move to the bundle directory and run the following command:
+``` bash
+dapr init --from-dir .
+```
+> For linux users, if you run your docker cmds with sudo, you need to use "**sudo dapr init**" 
+
+> If you are not running the above cmd from the bundle directory, provide the full path to bundle directory as input. e.g. assuming bundle directory path is $HOME/daprbundle, run `dapr init --from-dir $HOME/daprbundle` to have the same behavior.
+
+
+Output should look like as follows:
+```bash
+  Making the jump to hyperspace...
+ℹ️  Installing runtime version latest
+↘  Extracting binaries and setting up components... Loaded image: daprio/dapr:$version
+✅  Extracting binaries and setting up components...
+✅  Extracted binaries and completed components set up.
+ℹ️  daprd binary has been installed to $HOME/.dapr/bin.
+ℹ️  dapr_placement container is running.
+ℹ️  Use `docker ps` to check running containers.
+✅  Success! Dapr is up and running. To get started, go here: https://aka.ms/dapr-getting-started
+```
+> Note: To see that Dapr has been installed successfully, from a command prompt run the `docker ps` command and check that the `daprio/dapr:$version` container is up and running.
+
+This step creates the following defaults:
+
+1. components folder which is later used during `dapr run` unless the `--components-path` option is provided. For Linux/MacOS, the default components folder path is `$HOME/.dapr/components` and for Windows it is `%USERPROFILE%\.dapr\components`.
+2. component files in the components folder called `pubsub.yaml` and `statestore.yaml`.
+3. default config file `$HOME/.dapr/config.yaml` for Linux/MacOS or for Windows at `%USERPROFILE%\.dapr\config.yaml` to enable tracing on `dapr init` call. Can be overridden with the `--config` flag on `dapr run`.
+
+> Note: To emulate *online* dapr initialization using `dapr init`, you can also run redis/zipkin containers as follows:
+```
+1. docker run --name "dapr_zipkin" --restart always -d -p 9411:9411 openzipkin/zipkin
+2. docker run --name "dapr_redis" --restart always -d -p 6379:6379 redislabs/rejson
+```
+
+#### Slim Init
+Alternatively to the above, to have the CLI not install any default configuration files or run Docker containers, use the `--slim` flag with the init command. Only Dapr binaries will be installed.
+
+``` bash
+dapr init --slim --from-dir .
+```
+
+Output should look like this:
+```bash
+⌛  Making the jump to hyperspace...
+ℹ️  Installing runtime version latest
+↙  Extracting binaries and setting up components... 
+✅  Extracting binaries and setting up components...
+✅  Extracted binaries and completed components set up.
+ℹ️  daprd binary has been installed to $HOME.dapr/bin.
+ℹ️  placement binary has been installed to $HOME/.dapr/bin.
+✅  Success! Dapr is up and running. To get started, go here: https://aka.ms/dapr-getting-started
+```
+
+>Note: When initializing Dapr with the `--slim` flag only the Dapr runtime binary and the placement service binary are installed. An empty default components folder is created with no default configuration files. During `dapr run` user should use `--components-path` to point to a components directory with custom configurations files or alternatively place these files in the default directory. For Linux/MacOS, the default components directory path is `$HOME/.dapr/components` and for Windows it is `%USERPROFILE%\.dapr\components`.
+
